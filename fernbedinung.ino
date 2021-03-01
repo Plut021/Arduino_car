547697da58d4d3f02fe611a8bbd047846c5b3879
Plut021 <68148587+Plut021@users.noreply.github.com>
2021-03-01 09:11:05 +0100

    Update fernbedinung.ino

--- fernbedinung.ino
+++ fernbedinung.ino
@@ -1,8 +1,29 @@
-#include <IRremote>;
+#include <IRremote.h>
 
-int FREQUENZE = 30;
+int FREQUENZE = 30; // in  kHz
 int IR_RECIVE_PIN = 10;
+int STICK_X = A0;
+int STICK_Y = A1;
+int x_val;
+int y_val;
 
-uint16_t sAdress = 0x102;
-uint8_t /* asign in funktion
 
+uint16_t ADRESS = 0x102;
+uint8_t REPEATS = 0;
+
+
+void Send (uint8_t Command){
+    IrSender.sendRC6(ADRESS, Command, REPEATS);
+}
+
+void setup(){
+    Serial.begin(9600);
+
+}
+
+void loop() {
+    Serial.print("start");
+    x_val = analogRead(STICK_X);
+    y_val = analogRead(STICK_Y);
+    Serial.print(x_val, y_val);
+}
