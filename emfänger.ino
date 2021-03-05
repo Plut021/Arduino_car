971655456648ff7d5b924d4de985f4d6a35b6e0c
Plut021 <68148587+Plut021@users.noreply.github.com>
2021-03-05 12:17:51 +0100

    Create emfänger.ino

---
+++ emfänger.ino
@@ -0,0 +1,16 @@
+#include <IRremote.h>
+
+int IR_RECIVE_PIN = 10;
+
+void setup(){
+    IrReciver.begin(IR_RECIVE_PIN);
+    Serial.begin(9600);
+}
+
+void emfangen(){
+    if (IrReciver.decode()){
+        IrReciver.printIRResultShort(&Serial);
+        IrReciver.resume();
+        
+    }
+}
