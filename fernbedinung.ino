35d03bd7a103e3ee25a6537d1c18e87aca5927ac
Plut021 <68148587+Plut021@users.noreply.github.com>
2021-03-12 11:35:08 +0100

    Update fernbedinung.ino

--- fernbedinung.ino
+++ fernbedinung.ino
@@ -1,33 +1,68 @@
 #include <IRremote.h>
 
 
-int IR_SEND_PIN = 10;
+int IR_SEND = 3;
 int STICK_X = A0;
 int STICK_Y = A1;
 int x_val;
 int y_val;
+String komplett;
+String x_pos;
+String y_pos;
+int msg;
+String zero = "0";
+String add;
+uint8_t msg_hex;
+String komplett_hex;
+
 
 
 uint16_t ADRESS = 0x102;
 uint8_t REPEATS = 0;
 
 
-void sending (uint8_t Command){
+void sending (){
     //x_val = analogRead(STICK_X);
     //y_val = analogRead(STICK_Y);
+    x_val = 1563;
+    y_val = 5655;
+    x_pos = String(x_pos);
+    y_pos = String(y_pos);
+    add = "";
+    if(x_pos.length() < 4){
+        for(int i = 0; i < 4 - x_pos.length(); i++){
+            add += zero;
+        }
+        x_pos = add + x_pos;
+    }
+    add = "";
+    if(y_pos.length() < 4){
+        for(int i = 0; i < 4 - y_pos.length(); i++){
+            add += zero;
+        }
+        y_pos = add + y_pos;
+    }
     
-    IrSender.sendRC6(ADRESS, Command, REPEATS);
+    String komplett = x_pos + y_pos;
+    komplett_hex = String(komplett.toInt(), HEX);
+    msg_hex = uint8_t komplett_hex;
+    Serial.println(msg_hex);
+
+    
+
+    
+    IrSender.sendRC5(ADRESS, komplett_hex, REPEATS);
 }
 
 void setup(){
-    IrSender.begin(IR_SEND_PIN);
+    IrSender.begin(IR_SEND);
     Serial.begin(9600);
 
 }
 
 void loop() {
-    sending(0x15)
-    delay(1000)
-    Serial.println("sending done")
+    sending();
+    delay(1000);
+    Serial.println("sending done");
     
 }
