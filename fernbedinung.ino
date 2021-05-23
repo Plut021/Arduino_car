6bb0adc8e401b609dec97b464f502a211f5e176d
Plut021 <68148587+Plut021@users.noreply.github.com>
2021-05-23 13:55:58 +0200

    removed direct position sending

--- fernbedinung.ino
+++ fernbedinung.ino
@@ -6,14 +6,6 @@ const int STICK_X = A0;
 const int STICK_Y = A1;
 int x_val;
 int y_val;
-String komplett;
-String x_pos;
-String y_pos;
-int msg;
-String zero = "0";
-String add;
-uint8_t msg_hex;
-String komplett_hex;
 uint8_t command;
 
 
@@ -21,39 +13,6 @@ uint8_t command;
 const uint16_t ADRESS = 0x102;
 const uint8_t REPEATS = 0;
 
-// funktion wird verwendet wenn 32 bit zahlen übertragen werden können
-void sending (){
-    //x_val = analogRead(STICK_X);
-    //y_val = analogRead(STICK_Y);
-    x_val = 1563;
-    y_val = 5655;
-    x_pos = String(x_pos);
-    y_pos = String(y_pos);
-    add = "";
-    if(x_pos.length() < 4){
-        for(int i = 0; i < 4 - x_pos.length(); i++){
-            add += zero;
-        }
-        x_pos = add + x_pos;
-    }
-    add = "";
-    if(y_pos.length() < 4){
-        for(int i = 0; i < 4 - y_pos.length(); i++){
-            add += zero;
-        }
-        y_pos = add + y_pos;
-    }
-    
-    String komplett = x_pos + y_pos;
-    komplett_hex = String(komplett.toInt(), HEX);
-    msg_hex = komplett_hex.toInt();
-    Serial.println(komplett);
-    Serial.println(msg_hex);
-    
-
-    
-    IrSender.sendRC5(ADRESS, msg_hex, REPEATS);
-}
 
 //wenn direkte werte übertragungs probleme verursachen 
 void sending2(){
